f3bdcf5caf62985c71697bf2d22b187e3ba015a9
Silas Owu <owusilasyp00@gmail.com>
2024-09-24 03:56:49 -0700

    🚚 Chore: refact routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,39 +1,52 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
+import { AuthComponent } from './views/auth/auth.component';
 
 export const routes: Routes = [
+  // Default route
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
+
+  // Auth routes
   {
-    path:'**',
-    component: LoginComponent,
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      {
+        path: 'signup',
+        loadComponent: () => import('./components/auth/sign-up/sign-up.component').then(mod => mod.SignUpComponent)
+      }
+    ]
   },
+
+  // Main app routes
   {
-    path: 'signup',
-    loadComponent: ()=>
-    import('./components/auth/sign-up/sign-up.component')
-    .then(mod => mod.SignUpComponent),
+    path: 'app',
+    loadComponent: () => import('./views/homepage/homepage.component')
+    .then(mod => mod.HomepageComponent),
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./views/home-dashboard/home-dashboard.component').then(mod => mod.HomeDashboardComponent)
+      },
+      {
+        path: 'movies',
+        loadComponent: () => import('./views/movies-dashboard/movies-dashboard.component').then(mod => mod.MoviesDashboardComponent)
+      },
+      {
+        path: 'tv-series',
+        loadComponent: () => import('./views/series-dashboard/series-dashboard.component').then(mod => mod.SeriesDashboardComponent)
+      },
+      {
+        path: 'bookmarks',
+        loadComponent: () => import('./views/bookmarked-dashboard/bookmarked-dashboard.component').then(mod => mod.BookmarkedDashboardComponent)
+      }
+    ]
   },
-  {
-    path: 'homepage',
-    loadComponent: ()=>
-    import('./views/home-dashboard/home-dashboard.component')
-    .then(mod => mod.HomeDashboardComponent),
-  },
-  {
-    path: 'movies',
-    loadComponent: ()=>
-    import('./views/movies-dashboard/movies-dashboard.component')
-    .then(mod => mod.MoviesDashboardComponent),
-  },
-  {
-    path: 'tv-series',
-    loadComponent: ()=>
-    import('./views/series-dashboard/series-dashboard.component')
-    .then(mod => mod.SeriesDashboardComponent)
-  },
-  {
-    path: 'bookmarks',
-    loadComponent: ()=>
-    import('./views/bookmarked-dashboard/bookmarked-dashboard.component')
-    .then(mod => mod.BookmarkedDashboardComponent)
-  }
+
+  // Wildcard route for 404
+  // {
+  //   path: '**',
+  //   loadComponent: () => import('./components/not-found/not-found.component').then(mod => mod.NotFoundComponent)
+  // }
 ];
--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
+
 export class LoginComponent {
 
 }
