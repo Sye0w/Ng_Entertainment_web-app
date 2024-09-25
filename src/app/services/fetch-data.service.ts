402b446eceb0f3feb26f499d898e91f35803d5f9
Silas Owu <owusilasyp00@gmail.com>
2024-09-25 06:37:26 -0700

    🌐 Chore: add fetch service to load data

---
+++ src/app/services/fetch-data.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IMediaItem } from '../store/media.interface';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FetchDataService {
+  private apiUrl: string = '../../assets/data.json';
+
+  constructor(private http: HttpClient) { }
+
+  fetchData(): Observable<IMediaItem[]>{
+    return this.http.get<IMediaItem[]>(this.apiUrl);
+  }
+}
---
+++ src/app/store/media.interface.ts
@@ -0,0 +1,19 @@
+export interface IMediaItem {
+  title: string;
+  thumbnail: {
+    trending?: {
+      small: string;
+      large: string;
+    };
+    regular: {
+      small: string;
+      medium: string;
+      large: string;
+    };
+  };
+  year: number;
+  category: "Movie" | "TV Series";
+  rating: "E" | "PG" | "18+";
+  isBookmarked: boolean;
+  isTrending: boolean;
+}
