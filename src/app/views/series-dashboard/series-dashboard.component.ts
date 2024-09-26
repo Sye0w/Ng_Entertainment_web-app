3b904b580ca7c2ba835ac588046c68f4792c6a65
Silas Owu <owusilasyp00@gmail.com>
2024-09-26 15:17:17 -0700

    🎨 Style: add ui for series dashboard

--- src/app/views/series-dashboard/series-dashboard.component.ts
+++ src/app/views/series-dashboard/series-dashboard.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MediaFacadeService } from '../../services/media-facade.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-series-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './series-dashboard.component.html',
   styleUrl: './series-dashboard.component.scss'
 })
-export class SeriesDashboardComponent {
 
+export class SeriesDashboardComponent {
+  series$ = this.mediaFacade.series$
+  constructor(private mediaFacade: MediaFacadeService){}
 }
