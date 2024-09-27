7609ead0f2e906ea85216c4de83f4f3f2cda7f54
Silas Owu <owusilasyp00@gmail.com>
2024-09-27 01:31:26 -0700

    feat: setup store with localstorage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,23 +5,15 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { localStorageSync } from 'ngrx-store-localstorage';
 import { MediaEffects } from './store/media/media.effects';
 import { mediaReducer } from './store/media/media.reducer';
-
-export function localStorageSyncReducer(reducer: any) {
-  return localStorageSync({
-    keys: ['medias'],
-    rehydrate: true
-  })(reducer);
-}
+import { metaReducers } from './store/meta-reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideStore(localStorageSyncReducer),
-    provideState({ name: 'medias', reducer: mediaReducer }),
+    provideStore({ medias: mediaReducer }, { metaReducers }),
     provideEffects(MediaEffects),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
   ]
