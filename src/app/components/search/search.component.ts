ebdb063a8603bc24f292a6bd7b60ebe69331c60c
Silas Owu <owusilasyp00@gmail.com>
2024-09-27 04:22:21 -0700

    👔 Feat: implement search functionality through routes query params

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
+import { Subject, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -8,8 +11,39 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
+export class SearchComponent implements OnInit, OnDestroy {
+  searchTerm: string = '';
+  private searchTerms = new Subject<string>();
+  private destroy$ = new Subject<void>();
 
-export class SearchComponent {
-  title: string=''
+  constructor(
+    private store: Store,
+    private router: Router
+  ) {}
 
+  ngOnInit() {
+    this.setupSearch();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  onSearchInput() {
+    this.searchTerms.next(this.searchTerm);
+  }
+
+  private setupSearch() {
+    this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      takeUntil(this.destroy$)
+    ).subscribe(term => {
+      this.router.navigate([], {
+        queryParams: { search: term || null },
+        queryParamsHandling: 'merge'
+      });
+    });
+  }
 }
