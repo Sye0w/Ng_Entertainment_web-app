03b2e583a2475faf2910000af769bacad2b19c34
Silas Owu <owusilasyp00@gmail.com>
2024-09-27 04:24:55 -0700

    🧱 Chore: update store setup with router store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,28 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideState, provideStore } from '@ngrx/store';
+import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { MediaEffects } from './store/media/media.effects';
 import { mediaReducer } from './store/media/media.reducer';
 import { metaReducers } from './store/meta-reducer';
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+import { appReducers } from './store/reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(),
-    provideStore({ medias: mediaReducer }, { metaReducers }),
+    provideStore(
+      {
+        ...appReducers,
+        router: routerReducer
+      },
+      { metaReducers }
+    ),
+    provideRouterStore(),
     provideEffects(MediaEffects),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
   ]
