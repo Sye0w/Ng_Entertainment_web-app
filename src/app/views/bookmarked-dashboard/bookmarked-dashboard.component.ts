75b40ef5b2451a25cdec77a35bc05cc907a16daf
Silas Owu <owusilasyp00@gmail.com>
2024-09-26 17:04:51 -0700

    💄 Style: add ui for bookmarks dashboard

--- src/app/views/bookmarked-dashboard/bookmarked-dashboard.component.ts
+++ src/app/views/bookmarked-dashboard/bookmarked-dashboard.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MediaFacadeService } from '../../services/media-facade.service';
+import { map, Observable } from 'rxjs';
+import { IMediaItem } from '../../store/media.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bookmarked-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './bookmarked-dashboard.component.html',
   styleUrl: './bookmarked-dashboard.component.scss'
 })
-export class BookmarkedDashboardComponent {
 
+export class BookmarkedDashboardComponent implements OnInit {
+  bookmarks$: Observable<IMediaItem[]> = this.mediaFacade.bookmarks$
+  bookmarkedMovies$!: Observable<IMediaItem[]>;
+  bookmarkedSeries$!: Observable<IMediaItem[]>;
+  
+  constructor(private mediaFacade: MediaFacadeService){}
+
+  ngOnInit(){
+    this.bookmarkedMovies$ = this.bookmarks$.pipe(
+      map((bookmarks: IMediaItem[]) => bookmarks.filter((item) => item.category === 'Movie'))
+    );
+
+    this.bookmarkedSeries$ = this.bookmarks$.pipe(
+      map((bookmarks: IMediaItem[]) => bookmarks.filter((item) => item.category === 'TV Series'))
+    );
+  }
 }
